0dff3835875adb183d722a500ebd7d451a373d48
Thomas Heaney <99109758+tommyheaney@users.noreply.github.com>
2024-05-21 22:23:18 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentDatabase.Models;
+
+namespace StudentDatabase
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=StudentDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StudentDatabase.Models;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                // Ensure database is created
+                context.Database.EnsureCreated();
+
+                // Add a new student
+                var student = new Student
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    DateOfBirth = new DateTime(2000, 1, 1)
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                Console.WriteLine("Student added successfully.");
+            }
+        }
+    }
+}
